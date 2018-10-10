9dd5bae7685036df59096e0b9f70608d92b91151
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-10 19:25:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,11 @@
 def speak_to_grandma(phrase)
   if "#{phrase}" == "#{phrase}".downcase
     return "HUH?! SPEAK UP, SONNY!"
-  end
+ 
   elsif "#{phrase}" == "#{phrase}".upcase
     return "NO, NOT SINCE 1938"
-  end
+
   else "phrase" == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  end
+  
 end 
